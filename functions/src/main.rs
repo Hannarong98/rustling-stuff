8252120f1d3fcf354c4d49ef7d6e28008d504e2d
Hannarong Klinjan <hannarong98@hotmail.com>
2023-03-13 23:33:59 +0100

    feat: complete functions

---
+++ functions/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    println!("Hello, world!");
+
+    do_math(2, 3);
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn sub(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+fn mul(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+fn do_math(a: i32, b: i32) {
+    let result: (i32, i32, i32) = (add(a, b), sub(a, b), mul(a, b));
+
+    println!("(add, sub, mul) : {:?}", result);
+}
